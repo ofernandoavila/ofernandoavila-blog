1b633a656236b4113cea3395c7a4a8d08d6f1f37
Fernando Avila <fernandoavilajunior@gmail.com>
2025-01-29 13:40:07 -0300

    Project cleaning

--- index.php
+++
@@ -1,4 +0,0 @@
-<?php get_header(); ?>
-<div id="teste-123">123123213</div>
-<div id="root"></div>
-<?php get_footer(); ?>
--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,7 @@
 import { defineConfig } from 'vite'
-import { wordpress } from "wordpress-vite-plugin";
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [
-        wordpress({
-            input: [
-                'src/main.tsx'
-            ],
-            refresh: [
-                '**.php',
-                '**.vue',
-                '**.css',
-            ],
-            namespace: "theme-vite",
-            ssrExternal: true
-        }),
-    ],
-    server: {
-        headers: {
-            'Access-Control-Allow-Origin': '*', // Permite qualquer origem (para desenvolvimento)
-            'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS',
-            'Access-Control-Allow-Headers': 'X-Requested-With,Content-Type,Authorization',
-        },
-    }
+	plugins: [react()]
 })
